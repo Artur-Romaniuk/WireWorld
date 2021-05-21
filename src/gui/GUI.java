e96d6eeb7f10e7e9429d6732f1f1c855ab35fc3b
KizuPL <KiczuPL@outlook.com>
2021-05-21 17:55:10 +0200

    ontrol panel rework

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -21,8 +21,9 @@ public class GUI {
     private JButton drawButton;
     private JButton openFileButton;
 
-    private JTextField setNumberOfIterationsTextField;
-    private JTextField terminalTextField;
+    private JSpinner setNumberOfIterationsTextField;
+    private JTextArea terminalTextArea;                 //parser input
+    private JLabel errTextField;                        //parser error output
 
     private Dimension size;
     private int width;
@@ -36,7 +37,7 @@ public class GUI {
         mainPanel = new JPanel();
 
         frame = new JFrame();
-        frame.setSize(width/2, height/2);
+        frame.setSize(width / 2, height / 2);
         frame.add(mainPanel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("WireWorld");
@@ -44,7 +45,7 @@ public class GUI {
         mainPanel.setLayout(new BorderLayout());
 
         showControlPanel();
-        mainPanel.add(controlPanel, BorderLayout.NORTH);
+        mainPanel.add(controlPanel, BorderLayout.EAST);
 
         showWWBoard(10);
         mainPanel.add(boardPanel, BorderLayout.CENTER);
@@ -55,33 +56,91 @@ public class GUI {
         frame.setVisible(true);
     }
 
-    private void showControlPanel(){
+    private void showControlPanel() {
         controlPanel = new JPanel();
-        controlPanel.setLayout(new FlowLayout());
-        controlPanel.setMaximumSize(new Dimension(width/2, height));
+        controlPanel.setLayout(new GridBagLayout());
+        controlPanel.setMaximumSize(new Dimension(width / 2, height));
+
+        JPanel controlSubPanel1 = new JPanel();
+        JPanel controlSubPanel2 = new JPanel();
+        JScrollPane terminalScrollPanel;
+
+        controlSubPanel1.setLayout(new GridBagLayout());
+        controlSubPanel2.setLayout(new GridBagLayout());
 
         startButton = new JButton("Start");
         saveButton = new JButton("Save");
 
-        setNumberOfIterationsTextField = new JTextField("Number of iterations", 15);
+        SpinnerModel values = new SpinnerNumberModel(1,0,99999,1);
+        setNumberOfIterationsTextField = new JSpinner(values);
 
         drawButton = new JButton("Draw");
         openFileButton = new JButton("Open file");
 
-        terminalTextField = new JTextField("Terminal", 15);
+        terminalTextArea = new JTextArea("Terminal",22,15);
+        terminalScrollPanel = new JScrollPane(terminalTextArea);
 
-        controlPanel.add(startButton, BorderLayout.EAST);
-        controlPanel.add(saveButton, BorderLayout.EAST);
-        controlPanel.add(setNumberOfIterationsTextField, BorderLayout.EAST);
-        controlPanel.add(drawButton, BorderLayout.EAST);
-        controlPanel.add(openFileButton, BorderLayout.EAST);
-        controlPanel.add(terminalTextField, BorderLayout.EAST);
+        GridBagConstraints gbc1 = new GridBagConstraints();
+        gbc1.fill = GridBagConstraints.HORIZONTAL;
+        gbc1.insets = new Insets(3, 3, 3, 3);
+
+        gbc1.gridx = 0;
+        gbc1.gridy = 0;
+        controlSubPanel1.add(startButton, gbc1);
+
+        gbc1.gridx = 1;
+        gbc1.gridy = 0;
+        controlSubPanel1.add(saveButton, gbc1);
+
+        gbc1.gridx = 0;
+        gbc1.gridy = 1;
+        controlSubPanel1.add(new JLabel("Iterations:"), gbc1);
+
+        gbc1.gridx = 1;
+        gbc1.gridy = 1;
+        controlSubPanel1.add(setNumberOfIterationsTextField, gbc1);
+
+        GridBagConstraints gbc2 = new GridBagConstraints();
+        gbc2.fill = GridBagConstraints.HORIZONTAL;
+        gbc2.insets = new Insets(3, 3, 3, 3);
+
+        gbc2.gridx = 0;
+        gbc2.gridy = 0;
+        controlSubPanel2.add(drawButton, gbc2);
+
+        gbc2.gridx = 1;
+        gbc2.gridy = 0;
+        controlSubPanel2.add(openFileButton, gbc2);
+
+        errTextField = new JLabel();
+        errTextField.setForeground(Color.RED);
+        gbc2.gridwidth = 2;
+        gbc2.gridx = 0;
+        gbc2.gridy = 1;
+        controlSubPanel2.add(errTextField, gbc2);
+
+        gbc2.gridwidth = 2;
+        gbc2.gridx = 0;
+        gbc2.gridy = 2;
+        controlSubPanel2.add(terminalScrollPanel, gbc2);
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(3, 3, 3, 3);
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        controlPanel.add(controlSubPanel1, gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        controlPanel.add(controlSubPanel2, gbc);
+
     }
 
-    private void showWWBoard(int boardSize){
+    private void showWWBoard(int boardSize) {
         boardPanel = new JPanel();
-        boardPanel.setLayout(new GridLayout(10,10));
-       // boardPanel.setSize(650,650);
+        boardPanel.setLayout(new GridLayout(10, 10));
+        // boardPanel.setSize(650,650);
         //boardPanel.setMaximumSize(new Dimension(3*width/4, 3*height/2));
         boardPanel.setVisible(true);
 
@@ -89,11 +148,10 @@ public class GUI {
         JButton gridButton = null;
         jButtonList = new ArrayList<JButton>();
 
-        for(int i = 0; i< boardSize*boardSize; i++)
-        {
+        for (int i = 0; i < boardSize * boardSize; i++) {
             gridButton = new JButton();
             gridButton.setBackground(blackColor);
-            gridButton.setPreferredSize(new Dimension(10,10));
+            gridButton.setPreferredSize(new Dimension(10, 10));
             gridButton.setBorderPainted(true);
             gridButton.setRolloverEnabled(false);
             jButtonList.add(gridButton);
